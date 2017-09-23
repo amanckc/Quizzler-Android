6d534510056d3de4f7f51a3991ecd825f6747989
amanckc <aman96.mit@gmail.com>
2017-09-24 00:31:08 +0530

    Second phase done..

--- app/src/main/java/com/londonappbrewery/quizzler/MainActivity.java
+++ app/src/main/java/com/londonappbrewery/quizzler/MainActivity.java
@@ -61,7 +61,7 @@ public class MainActivity extends Activity {
             @Override
             public void onClick (View v) {
                 checkAnswer (false);
-               updateQuestion ();
+               updateQuestion (); 
             }
         });
 
